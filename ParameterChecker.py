6e134aed8c1f3a6f25fd6530240e13eb38a6b84b
bm600 <blakemcgee60@gmail.com>
2025-05-28 12:20:54 -0600

    check_file_format function added within ParameterChecker class

--- ParameterChecker.py
+++ ParameterChecker.py
@@ -3,17 +3,15 @@ import os
 class ParameterChecker:
     '''A class to check the validity of parameters for TSP rule generation.'''
 
-    def __init__(self, parameters):
-        self.parameters = parameters
+    def __init__(self, quant_path, meta_path):
+        self.quant_path = quant_path
+        self.meta_path = meta_path
 
-    def check(self):
-        if not isinstance(self.parameters, dict):
-            raise ValueError("Parameters should be a dictionary.")
+    def check_file_format(self):
+        '''Check if the file format is valid based on whether the extension is "csv" or "tsv".'''
+        lower_name = os.path.basename(self.quant_path).lower().split('.')
+
+        if lower_name[1] not in ['csv', 'tsv']:
+            raise ValueError(f"Could not determine file type: '{self.file_path}'. Expected 'csv' or 'tsv' file extension.")
         
-        for key, value in self.parameters.items():
-            if not isinstance(key, str):
-                raise ValueError(f"Parameter key '{key}' must be a string.")
-            if not isinstance(value, (str, int, float)):
-                raise ValueError(f"Parameter value for '{key}' must be a string, int, or float.")
         
-        return True
