29a77235559e7b04a3966df1acc93846116d83bc
bm600 <blakemcgee60@gmail.com>
2025-05-28 13:33:24 -0600

    Added DataTableChecker Class

---
+++ DataTableChecker.py
@@ -0,0 +1,19 @@
+class DataTableChecker:
+    def __init__(self, quant_file: str, meta_file: str):
+        self.quant_file = quant_file
+        self.meta_file = meta_file
+
+    def check_meta_file(self):
+        pass
+    
+    def check_samples(self):
+        pass
+
+    def check_quant_data(self):
+        pass
+
+    def check_duplicate_proteins(self):
+        pass
+
+    def check_duplicate_samples(self):
+        pass
