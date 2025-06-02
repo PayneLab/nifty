46060a2739423d2b9eebb6c58f28144703bf5ee2
benecharry <68670652+benecharry@users.noreply.github.com>
2025-06-02 10:43:13 -0600

    Added check_duplicate_samples implementation. Finished check_samples.

--- DataTableChecker.py
+++ DataTableChecker.py
@@ -3,12 +3,13 @@ class DataTableChecker:
         pass
 
     def check_meta_file(self, meta_df):
+        # Check if there are two columns in the meta file.
         header = list(meta_df.columns)
-        # Meta data file has to have two columns
+        # The metadata file has to have two columns
         if len(header) != 2:
             print(f"Meta data file must have 2 columns, got {len(header)}.")
             return 1
-        # Meta data file's first column has to be named sample_id
+        # The metadata file's first column has to be named sample_id
         if header[0].strip() != "sample_id":
             print(f"First column of meta data file must be named 'sample_id', but found '{header[0]}'.")
             return 2
@@ -18,23 +19,46 @@ class DataTableChecker:
         return 0
 
     def check_samples(self, quant_df, meta_df):
+        # Check if there is the same number of samples in both files.
         quant_header = list(quant_df.columns)
-
         # Same as the previous function, but in the quant_df.
         if quant_header[0].strip() != "sample_id":
             print(f"First column of quant data file must be named 'sample_id', but found '{quant_header[0]}'.")
-            return 2
+            return 1
         # Quant df and meta df do not have the same number of rows.
         if len(quant_df) != len(meta_df):
             print(f"Number of rows in quant data file {len(quant_df)} does not match number of meta data file {len(meta_df)}")
+            return 2
+        # Quant df and meta df do not have the same sample IDs.
+        meta_ids = set(meta_df["sample_id"].astype(str).str.strip())
+        quant_ids = set(quant_df["sample_id"].astype(str).str.strip())
+
+        if quant_ids != meta_ids:
+            print(f"IDs in quant data file does not match IDs in meta data file.")
             return 3
         return 0
 
     def check_quant_data(self):
+        # Missing Implementation
         pass
 
-    def check_duplicate_proteins(self):
+    def check_duplicate_proteins(self, quant_df):
+        # Check for no duplicate protein names in quant files.
+
         pass
 
-    def check_duplicate_samples(self):
+    def check_duplicate_samples(self, quant_df, meta_df):
+        # Check that there are no duplicate sample IDs in both files.
+        if quant_df["sample_id"].duplicated().any():
+            print("Duplicate samples ID in quant data file.")
+            return 1
+        if meta_df["sample_id"].duplicated().any():
+            print("Duplicate samples ID in meta data file.")
+            return 2
+        return 0
+
+    def check_missingness(self):
+        pass
+
+    def check_enough_samples(self, quant_df, meta_df):
         pass
