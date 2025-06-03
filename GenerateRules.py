a06b0a4c7e41b0392a4bf63f059d171f4c9c88ab
bm600 <blakemcgee60@gmail.com>
2025-06-03 15:54:28 -0600

    Added generate rules class with associated Unit Test

---
+++ GenerateRules.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+import pandas as pd
+
+class GenerateRules:
+    def __init__(self):
+        pass
+
+    def get_protein_list(self, quant_df):
+        """Extracts the list of proteins from the quantification DataFrame."""
+
+        if 'sample_id' not in quant_df.columns:
+            return 1
+        
+        protein_list = list(quant_df.columns[1:])
+        return protein_list
+    
+    def generate_rule_pairs(self, quant_df):
+        """Generates all possible pairs of proteins from the quant df."""
+
+        protein_list = self.get_protein_list(quant_df)
+        rule_pairs = list(combinations(protein_list, 2))
+        return rule_pairs
+    
