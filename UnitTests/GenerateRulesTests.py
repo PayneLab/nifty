5935c2a2cbba3e0f466ff5608bc80cd5851403f8
bm600 <blakemcgee60@gmail.com>
2025-06-05 15:39:13 -0600

    Updated GenerateRulesTests

--- UnitTests/GenerateRulesTests.py
+++ UnitTests/GenerateRulesTests.py
@@ -14,31 +14,26 @@ class TestGenerateRules(unittest.TestCase):
     def setUp(self):
         self.generator = GenerateRules()
 
-    def test_get_protein_list_valid(self):
+    def test_generate_rules(self):
         df = pd.DataFrame(columns=['sample_id', 'P1', 'P2', 'P3'])
         expected = ['P1', 'P2', 'P3']
         result = self.generator.get_protein_list(df)
         self.assertEqual(result, expected)
 
-    def test_generate_rule_pairs_typical(self):
         df = pd.DataFrame(columns=['sample_id', 'P1', 'P2', 'P3'])
         expected_rules = [('P1', 'P2'), ('P1', 'P3'), ('P2', 'P3')]
         result = self.generator.generate_rule_pairs(df)
         self.assertEqual(result, expected_rules)
 
-    def test_generate_rule_pairs_minimum_proteins(self):
         df = pd.DataFrame(columns=['sample_id', 'P1'])
         expected_rules = []  # no pairs possible with only one protein
         result = self.generator.generate_rule_pairs(df)
         self.assertEqual(result, expected_rules)
 
-    def test_generate_rule_pairs_empty_df(self):
         df = pd.DataFrame(columns=['sample_id'])  # no proteins
         expected_rules = []
         result = self.generator.generate_rule_pairs(df)
         self.assertEqual(result, expected_rules)
-
-    # TODO make into one function
 
 if __name__ == '__main__':
     unittest.main()
