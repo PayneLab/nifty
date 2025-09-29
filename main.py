1575b49b9de4a203f2ecee0bf14d3e4bc8604999
Alyssa Nitz <71338312+nitz1bug@users.noreply.github.com>
2025-09-29 12:05:16 -0600

    updated using wrapper functions

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import pandas as pd
 
+from ParameterChecker import ParameterChecker
 from DataTableChecker import DataTableChecker
+from GenerateRules import GenerateRules
+from EvaluateRules import EvaluateRules
+
+
 
 
 def main():
+    # Check Parameters
+    param_checker = ParameterChecker()
+    args = []  # TODO
+
+    # Read in files
     meta_df = pd.read_csv("meta.tsv", sep="\t")
     quant_df = pd.read_csv("quant.tsv", sep="\t")
 
-    data_checker = DataTableChecker()
+    # Check Data Tables
+    data_table_checker = DataTableChecker()
+    filtered_quant_df, meta_df = data_table_checker.run_data_table_checker(args, quant_df, meta_df)
+
+    # Generate Rules
+    rule_generator = GenerateRules()
+    rules = rule_generator.generate_rule_pairs(filtered_quant_df)
+
+    # Evaluate Rules
+    rule_evaluator = EvaluateRules()
+    # TODO
 
 
 if __name__ == "__main__":
