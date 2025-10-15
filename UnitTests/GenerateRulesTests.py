a6ec4d5f7de7bf4906019dd4c090a05bec821061
Alyssa Nitz <71338312+nitz1bug@users.noreply.github.com>
2025-10-15 09:36:37 -0600

    fixed to match finalized functions

--- UnitTests/GenerateRulesTests.py
+++ UnitTests/GenerateRulesTests.py
@@ -17,7 +17,7 @@ class TestGenerateRules(unittest.TestCase):
     def test_generate_rules_return_all_proteins(self):
         # Get protein list.
         try:
-            df = pd.DataFrame(columns=['sample_id', 'P1', 'P2', 'P3'])
+            df = pd.DataFrame(columns=['P1', 'P2', 'P3'])
             expected = ['P1', 'P2', 'P3']
             self.assertEqual(self.generator.get_protein_list(df), expected)
         except Exception as e:
@@ -26,7 +26,7 @@ class TestGenerateRules(unittest.TestCase):
     def test_generate_rules_3_pairs(self):
         # Generate rules with 3 proteins.
         try:
-            df = pd.DataFrame(columns=['sample_id', 'P1', 'P2', 'P3'])
+            df = pd.DataFrame(columns=['P1', 'P2', 'P3'])
             expected_rules = [('P1', 'P2'), ('P1', 'P3'), ('P2', 'P3')]
             result = self.generator.generate_rule_pairs(df)
             self.assertEqual(result, expected_rules)
@@ -36,7 +36,7 @@ class TestGenerateRules(unittest.TestCase):
     def test_generate_rules_1_protein(self):
         # Generate one rule with only one protein.
         try:
-            df = pd.DataFrame(columns=['sample_id', 'P1'])
+            df = pd.DataFrame(columns=['P1'])
             expected_rules = []  # no pairs possible with only one protein
             result = self.generator.generate_rule_pairs(df)
             self.assertEqual(result, expected_rules)
@@ -46,7 +46,7 @@ class TestGenerateRules(unittest.TestCase):
     def test_generate_rules_no_proteins(self):
         # Generate no rules with no proteins.
         try:
-            df = pd.DataFrame(columns=['sample_id'])  # no proteins
+            df = pd.DataFrame(columns=[])  # no proteins
             expected_rules = []
             result = self.generator.generate_rule_pairs(df)
             self.assertEqual(result, expected_rules)
