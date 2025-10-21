e20dfe30ceb8d20c8f79b50632e772fc1cdb6d6d
nitz1bug <nitz1bug@byu.edu>
2025-10-21 08:53:06 -0600

    added save_model

--- ModelGenerator.py
+++ ModelGenerator.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import pickle
 
 from Colors import Colors
 from DataTransformer import DataTransformer
@@ -19,7 +20,8 @@ class ModelGenerator:
         pass
 
     def save_model(self, model, output_file_path):
-        pass
+        with open(output_file_path, "wb") as f:
+            pickle.dump(model, f)
         print(f"{Colors.INFO}INFO: Model saved to '{output_file_path}'.{Colors.END}", file=sys.stderr, flush=True)
 
     def save_performance_metrics(self, metrics, output_file_path):
@@ -47,10 +49,12 @@ class ModelGenerator:
         #       validate model
 
         # Save model to "trained_model.pkl" in the specified output dir (configs['output_dir'])
+        print("SAVING MODEL", file=sys.stderr, flush=True)
         model_output_path = os.path.join(configs['output_dir'], "trained_model.pkl")
         self.save_model(model=model, output_file_path=model_output_path)
 
         # Save train/validate information to "model_performance_metrics.???" in the specified output dir
+        print("SAVING MODEL PERFORMANCE METRICS", file=sys.stderr, flush=True)
         metrics_output_path = os.path.join(configs['output_dir'], "model_performance_metrics.txt")  # TODO: fix file extension when function is written
         self.save_performance_metrics(metrics=performance_metrics, output_file_path=metrics_output_path)
         
